58a334dd89d1ebef07d7487622cbdb05e9450b07
roy157 <78753915+roy157@users.noreply.github.com>
2026-09-20 09:33:30 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ async def mapear_motores_por_id():
         await client.connect()
 
     print("📋 Sincronizando e indexando IDs reales de Telegram...")
-    GRUPOS_A_OBVIAR = ["CANAL FRANCHESCO DATA SAC", "FRANCHESCO MASTER", "DF VIP [ GRUPO 05 ]"]
+    GRUPOS_A_OBVIAR = ["CANAL FRANCHESCO DATA SAC", "FRANCHESCO MASTER", "DF VIP [ GRUPO 08 ]"]
 
     async for dialog in client.iter_dialogs(limit=150):
         if dialog.name:
